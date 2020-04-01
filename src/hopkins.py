2fc032e3833fcc1c31e54202e3b3d0b0a30f55ba
mestydone <mestydone@gmail.com>
2020-04-01 16:47:51 +0300

    Improve performance

--- src/hopkins.py
+++ src/hopkins.py
@@ -2,11 +2,19 @@ import math
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 
-def _generate_points(length, dim):
-    generated = np.empty(length * dim)
-    for i in range(length*dim):
-        generated[i] =  (2 * np.random.random_sample() - 1) * math.sqrt(3)
-    return np.reshape(generated, (length,dim))
+# def _generate_points(length, dim, amin, amax):
+#     generated = np.empty(length * dim)
+#     for i in range(length*dim):
+#         generated[i] =  (2 * np.random.random_sample() - 1) * math.sqrt(3)
+#     return np.reshape(generated, (dim,length))
+
+def _generate_points(length, dim, amin, amax):
+    a = math.sqrt(3)
+    return np.random.uniform(-a, a, (length,dim)).reshape(dim,-1)
+
+
+# def _generate_points(length, dim, amin, amax):
+#     return np.random.uniform(amin, amax, (length,dim)).reshape(dim,-1)
 
 # сумма растояний от каждой точки до ее ближайшего соседа
 def _distance(points):
@@ -15,21 +23,38 @@ def _distance(points):
     distances, _ = nbrs.kneighbors(t_points)
     return distances[:,1].sum()
 
+def _std_dev(points):
+    mean = points.mean()
+    std = sum([pow(s - mean, 2) for s in points]) / len(points)
+    return math.sqrt(std)
+
+def _mean(points):
+    return sum(points) / len(points)
+
 def calculate(points):
-    length = len(points)
-    dim = len(points[0])
+    # print(points)
+    length = len(points[0])
+    dim = len(points)
 
     # Нормализация исходных данных
     real_norm_points = points.copy()
     for i in range(len(points)):
-        real_norm_points[i] -= real_norm_points[i].mean()
-        real_norm_points[i] /= np.std(real_norm_points[i])
+        real_norm_points[i] -= _mean(real_norm_points[i])
+        real_norm_points[i] /= _std_dev(real_norm_points[i])
     
     #Генерация случайных данных
-    generated_points = _generate_points(length, dim)
+    generated_points = _generate_points(length, dim, np.amin(real_norm_points, axis=1), np.amax(real_norm_points, axis=1))
 
     # Считаем расстояния в исходных и сгенерированных
     dist_real = _distance(real_norm_points)
     dist_generated = _distance(generated_points)
+
+    # print('var %.3f;' % np.var(real_norm_points), 'max %.3f;' % real_norm_points.max(), 
+    # 'min %.3f;' % real_norm_points.min(), 'avg %.3f;' % real_norm_points.mean())
+   
+    # print('var %.3f;' % np.var(generated_points), 'max %.3f;' % generated_points.max(), 
+    # 'min %.3f;' % generated_points.min(), 'avg %.3f;' % generated_points.mean())
+
+    # print(dist_real, dist_generated)
 
     return dist_generated / (dist_real + dist_generated)
--- src/mds.py
+++ src/mds.py
@@ -1,9 +1,6 @@
 import numpy as np
+from scipy.spatial import distance as scipy_distance
 import math
-
-# for perfomance estimation
-import time
-
 
 def init(points):
     return MDS_prepared(points)
@@ -22,19 +19,7 @@ class MDS_prepared:
     def __init__(self, points):
         self.points_transposed = points.transpose((1,0)).copy()
         self.points_len = len(self.points_transposed)
-        self.src_distances = np.empty((self.points_len, self.points_len))
-
-        for i in range(self.points_len):
-            self.src_distances[i] = self.get_distance(self.points_transposed, self.points_transposed[i])
-
-
-    # возвращает расстояния от point до всех точек из matr. E/e ij
-    def get_distance(self, points, to_point):
-        dist = np.zeros(len(points))
-        for i in range(0, len(points)):
-            dist[i] = np.linalg.norm(to_point - points[i]) # длина вектора
-
-        return dist
+        self.src_distances = scipy_distance.cdist(self.points_transposed, self.points_transposed)
 
     def calculate(self, dim):
         alpha = 4.7e-5 / self.points_len
@@ -43,65 +28,34 @@ class MDS_prepared:
 
     def scale(self, proj, alpha):
         counter = 0
-        prevDistance = 1e300
+        prev_distance = 1e300
         distance = 0
-        
-        time_common = time.time()
-        time_eij = 0
-        time_L = 0
-        time_scaling = 0
-
 
         while(counter < 100):
-            counter+=1
-            projDistance = []
+            counter += 1
 
+            # рассчет e_ij (расстояния между точками в проекции и разность расстояний с исходными)
+            proj_distance = scipy_distance.cdist(proj, proj)
+            points_distance = proj_distance - self.src_distances
 
-            # рассчет e_ij
-            start_time_eij = time.time()            
-            for i in range(0, self.points_len):
-                projDistance.append(self.get_distance(proj, proj[i]))
-            time_eij += time.time() - start_time_eijW
-
-            start_time_L = time.time()    
-            distance = 0 #L
-            for i in range(0, self.points_len):
-                for j in range(0, self.points_len):
-                    distance += pow(projDistance[j][i] - self.src_distances[j][i], 2)
-            time_L += time.time() - start_time_L
-
+            # рассчет разности сумм межточечных расстояний между точками в исходных данных и в проекции
+            distance = np.power(points_distance, 2).sum()
 
             if (
                 np.isnan(distance) 
                 or distance == 0 
-                or (prevDistance - distance) / pow(10, math.log10(distance)) < 0.01
+                or (prev_distance - distance) / pow(10, math.log10(distance)) < 0.01 # условие остановки
                 ):
                 break
 
             # двигаем точки
-            #alpha = 1.7e-10 / dataLen
-            start_time_scaling = time.time()    
-            newMatr = np.empty(self.points_len * len(proj[0])).reshape(len(proj), len(proj[0]))
+            corr_vectors = []
             for k in range(0, self.points_len):
-                point = proj[k]
-                newMatr[k] = point.copy()
-                vector = np.zeros(len(proj[0]))
-                for i in range(0, self.points_len):
-                    vector += (projDistance[i][k] - self.src_distances[i][k]) * (point - proj[i])
-                
-                vector *= alpha
-                newMatr[k] -= vector
+                point_vectors = (proj[k] - proj) * points_distance[:,k:k+1] # взвешенные вектора от точки k до остальных
+                corr_vectors.append(sum(point_vectors) * alpha)
 
-            time_scaling += time.time() - start_time_scaling
-
-            proj = newMatr
-            prevDistance = distance
-           # projSteps.append(proj)
-
-        print('t eij:', time_eij)
-        print('t L:', time_L)
-        print('t scaling:', time_scaling)
-        print('t common:', time.time() - time_common)
+            proj = proj - corr_vectors
+            prev_distance = distance
 
         return proj, distance
 
