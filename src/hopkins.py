problem with data normalization are fixed

src/hopkins.py
@@ -2,19 +2,25 @@ import math
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 
-# def _generate_points(length, dim, amin, amax):
+
+def _info(real_points, generated_points, real_dist, generated_dist):
+    print('var %.3f;' % np.var(real_points), 'max %.3f;' % real_points.max(), 
+    'min %.3f;' % real_points.min(), 'avg %.3f;' % real_points.mean())
+   
+    print('var %.3f;' % np.var(generated_points), 'max %.3f;' % generated_points.max(), 
+    'min %.3f;' % generated_points.min(), 'avg %.3f;' % generated_points.mean())
+
+    print(real_dist, generated_dist)
+
+# def _generate_points(length, dim):
 #     generated = np.empty(length * dim)
 #     for i in range(length*dim):
 #         generated[i] =  (2 * np.random.random_sample() - 1) * math.sqrt(3)
 #     return np.reshape(generated, (dim,length))
 
-def _generate_points(length, dim, amin, amax):
+def _generate_points(length, dim):
     a = math.sqrt(3)
     return np.random.uniform(-a, a, (length,dim)).reshape(dim,-1)
-
-
-# def _generate_points(length, dim, amin, amax):
-#     return np.random.uniform(amin, amax, (length,dim)).reshape(dim,-1)
 
 # сумма растояний от каждой точки до ее ближайшего соседа
 def _distance(points):
@@ -23,38 +29,24 @@ def _distance(points):
     distances, _ = nbrs.kneighbors(t_points)
     return distances[:,1].sum()
 
-def _std_dev(points):
-    mean = points.mean()
-    std = sum([pow(s - mean, 2) for s in points]) / len(points)
-    return math.sqrt(std)
-
-def _mean(points):
-    return sum(points) / len(points)
-
 def calculate(points):
-    # print(points)
     length = len(points[0])
     dim = len(points)
 
     # Нормализация исходных данных
     real_norm_points = points.copy()
     for i in range(len(points)):
-        real_norm_points[i] -= _mean(real_norm_points[i])
-        real_norm_points[i] /= _std_dev(real_norm_points[i])
-    
+        real_norm_points[i] -= np.mean(real_norm_points[i])
+    real_norm_points /= np.std(real_norm_points)
+
     #Генерация случайных данных
-    generated_points = _generate_points(length, dim, np.amin(real_norm_points, axis=1), np.amax(real_norm_points, axis=1))
+    generated_points = _generate_points(length, dim)
 
     # Считаем расстояния в исходных и сгенерированных
     dist_real = _distance(real_norm_points)
     dist_generated = _distance(generated_points)
-
-    # print('var %.3f;' % np.var(real_norm_points), 'max %.3f;' % real_norm_points.max(), 
-    # 'min %.3f;' % real_norm_points.min(), 'avg %.3f;' % real_norm_points.mean())
-   
-    # print('var %.3f;' % np.var(generated_points), 'max %.3f;' % generated_points.max(), 
-    # 'min %.3f;' % generated_points.min(), 'avg %.3f;' % generated_points.mean())
-
-    # print(dist_real, dist_generated)
+    
+    # Вывод информации о данных
+    # _info(real_norm_points, generated_points, dist_real, dist_generated)
 
     return dist_generated / (dist_real + dist_generated)

src/hubert.py
@@ -1,4 +1,5 @@
 from scipy.spatial import distance
+import math
 import numpy as np
 
 # Если определять кластеры самому
@@ -7,24 +8,26 @@ import numpy as np
 # clustering.fit(source_data.transpose((1,0)))
 # clustering.labels_
 
-
 def calculate(points, markers):
     N = len(points[0])
+    norm_points = points.copy()
 
-    t_points = points.copy().transpose((1,0))
+    # Нормализация исходных данных
+    for i in range(len(points)):
+        norm_points[i] -= np.mean(norm_points[i])
+    norm_points /= np.std(norm_points)
+
+    t_points = norm_points.copy().transpose((1,0))
     dist_mtrx = distance.cdist(t_points, t_points)
     conn_mtrx = np.zeros((N,N))
 
-    dist_mtrx -= dist_mtrx.min()
-    dist_mtrx /= dist_mtrx.max()
+    # Нормализация расстояний
+    dist_mtrx /= np.max(dist_mtrx)
 
     for i in range(1, N-1):
         for j in range(i+1, N):
             conn_mtrx[i][j] = markers[i] == markers[j]
 
-    hubert_sum = 0
-    for i in range(1, N-1):
-        for j in range(i+1, N):
-            hubert_sum += dist_mtrx[i][j] * conn_mtrx[i][j]
-
+    hubert_sum = np.sum(dist_mtrx * conn_mtrx)
+    
     return hubert_sum * 2 / (N*(N-1))